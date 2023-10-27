1510b87e52ced1ddade10871cef1218e2439112a
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-10-27 10:05:02 -0300

    utilização de modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {HeaderComponents} from "./components/header.component";
+import {ComponentsModule} from "./components/components.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,14 @@
+import { NgModule} from "@angular/core";
+import {HeaderComponents} from "./header.component";
+
+  @NgModule({
+    declarations: (
+      HeaderComponents
+    ),
+    exports: (
+      HeaderComponents
+    )
+  })
+export class ComponentsModule {
+
+  }
